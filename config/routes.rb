49f980d5c3a728dd1a0cc576b87d5053bb03d43b
Balint Erdi <balint.erdi@gmail.com>
2013-09-11 09:46:15 +0200

    Remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 TwitterClient::Application.routes.draw do
   get 'auth/twitter/callback', to: 'sessions#create'
-  resource   :session
 
-  resources :tweets, only: :create
-  resources :users, only: [:show, :create]
+  resource  :session,   only: [:new, :create]
   resources :timelines, only: [:show]
 
   root to: 'timelines#home'
